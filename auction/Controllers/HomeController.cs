691abaafc79984ce95b7f95d0c310b3079d98acf
Igli Janko <igli.janko@pragmatic.al>
2023-08-25 11:25:17 +0200

    Patch: bit amount

--- auction/Controllers/HomeController.cs
+++ auction/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ public class HomeController : Controller
 
         var bids = await _context.Bid
             .Where(b => b.BidderId == userId && _context.Bid
-                .Where(b2 => b2.ProductId == b.ProductId)
+                .Where(b2 => b2.ProductId == b.ProductId && b2.Product.isDeleted == false)
                 .Max(b2 => b2.Amount) == b.Amount)
             .ToListAsync();
         decimal onHold = 0;
@@ -162,7 +162,7 @@ public class HomeController : Controller
 
         var bids = await _context.Bid
             .Where(b => b.BidderId == model.UserId && _context.Bid
-                .Where(b2 => b2.ProductId == b.ProductId)
+                .Where(b2 => b2.ProductId == b.ProductId && b2.Product.isDeleted == false && b2.Transaction == null)
                 .Max(b2 => b2.Amount) == b.Amount)
             .ToListAsync();
         decimal onHold = 0;
