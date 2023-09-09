e2d789427dfd015450081f1415348c66b0a14be4
iglij <igli.janko@hotmail.com>
2023-09-09 12:14:22 +0200

    Update: Spinner on Product Details

--- auction/wwwroot/js/imgSpinner.js
+++ auction/wwwroot/js/imgSpinner.js
@@ -1,33 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
     let table = document.querySelector("table");
-
+    let row = document.querySelector(".rowDetails");
     if(table) {
         let rows = table.querySelectorAll("tr");
         
         rows.forEach(function(row) {
             processImageLoaders(table);
         });
-    } else {
-        console.log("Table does not exist.");
     }
+    else if(row){
+        const image = document.querySelector(".loading-image");
+        showSpinner(image);
+    }});
     
-});
-
 function processImageLoaders(parentElement) {
     const images = parentElement.querySelectorAll('.loading-image');
     images.forEach(img => {
-        const spinner = img.nextElementSibling;
-
-        if (img.complete) {
-            spinner.style.display = 'none';
-        } else {
-            spinner.style.display = 'block';
-        }
+        showSpinner(img);
     });
+}
+
+function showSpinner(img){
+    const spinner = img.nextElementSibling;;
+    if (img.complete) {
+        spinner.style.display = 'none';
+    } else {
+        spinner.style.display = 'block';
+    }
 }
 
 function hideSpinner(imgElement) {
     const spinner = imgElement.nextElementSibling;
+    
     if (spinner && spinner.classList.contains('loader')) {
         spinner.style.display = 'none';
     }
